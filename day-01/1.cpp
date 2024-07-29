5aec14c4f3add9d145199f6ca6a9cab432475cd8
Digvijay Narayan <digvijayn1993@gmail.com>
2024-07-29 14:31:56 +0530

    initial commit

---
+++ day-01/1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std; // determine scope of particular var(namespace)
+
+int main(){
+    cout<<"hello world"<<endl;
+
+}
---
+++ day-01/2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+// using std::cout;
+
+using namespace std;
+
+int main(){
+    cout<<"program 1\n";
+}
