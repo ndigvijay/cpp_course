b87b095285570218edcecc4ef20030fe6fa05fb4
Digvijay Narayan <digvijayn1993@gmail.com>
2024-07-29 14:37:24 +0530

    deleted files

--- 1.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std; // determine scope of particular var(namespace)
-
-int main(){
-    cout<<"hello world"<<endl;
-
-}
--- 2.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-// using std::cout;
-
-using namespace std;
-
-int main(){
-    cout<<"program 1\n";
-}
